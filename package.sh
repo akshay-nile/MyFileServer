43ff195ab43bf692b1f10d081a0fb62b8b0a903b
Akshay Nile <akshaynile8849@gmail.com>
2025-08-13 23:45:41 +0530

    updated package.sh

--- package.sh
+++ package.sh
@@ -6,10 +6,11 @@
 # 1) Creating the MyFileServer/libs folder (fresh)
 # 2) Building the frontend (dist folder)
 # 3) Moving dist to MyFileServer
-# 4) Copying backend python code files and requirements.txt
-# 5) Installing backend dependencies into libs
-# 6) Removing requirements.txt file
-# 7) Verifying the final structure
+# 4) Generating requirements.txt from pyproject.toml file
+# 5) Copying backend python code files and requirements.txt
+# 6) Installing backend dependencies into libs
+# 7) Removing requirements.txt file
+# 8) Verifying the final structure
 
 
 # --- SCRIPT SETUP ---
@@ -36,33 +37,35 @@ mkdir -p "$MY_FILE_SERVER_DIR/$SERVICES_DIR"
 
 # 2) Build frontend dist
 echo "Step 2: Building frontend..."
-cd "$FRONTEND_DIR"
+cd $FRONTEND_DIR
 npm run build
 
-# Back to root
-cd ..
-
 # 3) Move dist to MyFileServer
-echo "Step 3: Moving $FRONTEND_DIR/$DIST_DIR to $MY_FILE_SERVER_DIR..."
-mv "$FRONTEND_DIR/$DIST_DIR" "$MY_FILE_SERVER_DIR/"
+echo "Step 3: Moving $DIST_DIR to $MY_FILE_SERVER_DIR..."
+mv $DIST_DIR ../$MY_FILE_SERVER_DIR/
 
-# 4) Copy backend files (only .py from services)
-echo "Step 4: Copying backend files..."
-cp "$BACKEND_DIR/server.py" "$MY_FILE_SERVER_DIR/"
-cp "$BACKEND_DIR/$REQUIREMENTS_FILE" "$MY_FILE_SERVER_DIR/"
-find "$BACKEND_DIR/$SERVICES_DIR" -maxdepth 1 -name "*.py" -exec cp {} "$MY_FILE_SERVER_DIR/$SERVICES_DIR/" \;
+# 4) Generate requirements.txt file
+echo "Step 4: Generating requirements.txt file..."
+cd ../$BACKEND_DIR
+uv pip compile pyproject.toml -o $REQUIREMENTS_FILE --no-deps --no-annotate --no-header
 
-# 5) Install backend dependencies into libs
-echo "Step 5: Installing Python dependencies in libs..."
-cd "$MY_FILE_SERVER_DIR"
-pip install --target=$LIBS_DIR -r $REQUIREMENTS_FILE
+# 5) Copy backend files (only .py from services)
+echo "Step 5: Copying backend files..."
+mv $REQUIREMENTS_FILE ../$MY_FILE_SERVER_DIR/
+cp server.py ../$MY_FILE_SERVER_DIR/
+find $SERVICES_DIR -maxdepth 1 -name "*.py" -exec cp {} "../$MY_FILE_SERVER_DIR/$SERVICES_DIR/" \;
 
-# 6) Delete requirements.txt from MyFileServer
-echo "Step 6: Removing $REQUIREMENTS_FILE..."
-rm "$REQUIREMENTS_FILE"
+# 6) Install backend dependencies into libs
+echo "Step 6: Installing Python dependencies in libs..."
+cd ../$MY_FILE_SERVER_DIR
+uv pip install --target=$LIBS_DIR -r $REQUIREMENTS_FILE
 
-# 7) Verify structure
-echo "Step 7: Verifying final structure..."
+# 7) Delete requirements.txt from MyFileServer
+echo "Step 7: Removing $REQUIREMENTS_FILE..."
+rm $REQUIREMENTS_FILE
+
+# 8) Verify the final structure
+echo "Step 8: Verifying final structure..."
 if [ -d $SERVICES_DIR ] && \
    [ -d $DIST_DIR ] && \
    [ -d $LIBS_DIR ] && \
